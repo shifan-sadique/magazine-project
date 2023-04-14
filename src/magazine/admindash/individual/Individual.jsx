c5c2546024ce9a811d3e513fec5d42b25cfe2066
shifan-sadique <67517134+shifan-sadique@users.noreply.github.com>
2023-04-15 00:54:23 +0530

    individual page feature

--- src/magazine/admindash/individual/Individual.jsx
+++ src/magazine/admindash/individual/Individual.jsx
@@ -1,6 +1,7 @@
 import Sidebar from "../dashcomponent/dashSidebar/Dashsidebar"
 import Nav from "../dashcomponent/Nav/Nav"
 import "./Individual.scss"
+import Featured from "../dashcomponent/featured/Featured"
 
 
 function Individual() {
@@ -16,12 +17,35 @@ function Individual() {
             </div>
             <h1 className="title">User Information</h1>
             <div className="item">
-              <img className="itemImg" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-              Details
-            
+              <div className="details">
+                <h1 className="itemTitle">
+                  Nelwin George
+                </h1>
+                <img className="itemImg" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                <div className="detailitem">
+                  <span className="itemKey">Batch:</span>
+                  <span className="itemValue">21-23</span>
+                </div>
+                <div className="detailitem">
+                  <span className="itemKey">Specialization:</span>
+                  <span className="itemValue">Proof Read</span>
+                </div>
+                <div className="detailitem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">nellu@gmail.com</span>
+                </div>
+
+                <div className="detailitem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">9876543210</span>
+                </div>
+              </div>
+
             </div>
           </div>
-          <div className="right"></div>
+          <div className="right">
+              <Featured/>
+          </div>
         </div>
         <div className="bottom"></div>
       </div>
