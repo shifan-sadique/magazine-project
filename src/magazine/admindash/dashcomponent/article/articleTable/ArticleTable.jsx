db52c1b6ca0ec41ea1f8e28ec5b02ae442d213d2
shifan-sadique <67517134+shifan-sadique@users.noreply.github.com>
2023-05-17 16:55:31 +0530

    review form done

--- src/magazine/admindash/dashcomponent/article/articleTable/ArticleTable.jsx
+++ src/magazine/admindash/dashcomponent/article/articleTable/ArticleTable.jsx
@@ -1,6 +1,5 @@
-import './articleTable.scss'
-
-import React, { useState } from 'react';
+import './articleTable.scss';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -11,6 +10,8 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { collection, getDocs, onSnapshot } from 'firebase/firestore';
+import { db } from '../../../../../firebase';
 
 const useStyles = makeStyles({
   table: {
@@ -21,106 +22,95 @@ const useStyles = makeStyles({
 const List = () => {
   const classes = useStyles();
 
-  const rows=[
-    {
-      id:123,
-      date:"1 march 2023",
-      article_name:"Chat Gpt",
-      student_name:"shifan",
-      roll_no: "tve21mca2051",
-      type: "Article",
-      upvotes:3,
-      downvotes:2,
-      status: "Rejected",
-      approve:0,
-    },
-    {
-      id:124,
-      date:"1 apr 2023",
-      article_name:"Game",
-      student_name:"Shahban",
-      roll_no: "tve21mca2050",
-      type: "Photo",
-      upvotes:3,
-      downvotes:2,
-      status: "Approved",
-      approve:1,
-    },
-    {
-      id:127,
-      date:"1 june 2023",
-      article_name:"football",
-      student_name:"adwaith",
-      roll_no: "tve21mca2008",
-      type: "Article",
-      upvotes:3,
-      downvotes:5,
-      status: "Pending",
-      approve:0,
-    },
-
-  ]
-
+  const [rows, setRows] = useState([]);
   const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'article'));
+        const fetchedRows = querySnapshot.docs.map((doc) => doc.data());
+        setRows(fetchedRows);
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      }
+    };
+
+    const unsubscribe = onSnapshot(collection(db, 'article'), (snapshot) => {
+      const fetchedRows = snapshot.docs.map((doc) => doc.data());
+      setRows(fetchedRows);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleRead = (fileUrl) => {
+    // Create a temporary link element
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.target = '_blank';
+    link.download = ''; // Use the file name from the server if available
+
+    // Trigger the download
+    link.click();
+  };
 
   const handleSearch = (event) => {
     setSearchText(event.target.value);
   };
 
   const filteredRows = rows.filter((row) => {
+    const articleName = row.article_name || '';
+    const studentName = row.student_name || '';
     return (
-      row.article_name.toLowerCase().includes(searchText.toLowerCase()) ||
-      row.student_name.toLowerCase().includes(searchText.toLowerCase())
+      articleName.toLowerCase().includes(searchText.toLowerCase()) ||
+      studentName.toLowerCase().includes(searchText.toLowerCase())
     );
   });
 
   return (
-    <div className='table'>
+    <div className="table">
       <div className="search">
-        <TextField
-          id="search"
-          label="Search"
-          type="search"
-          value={searchText}
-          onChange={handleSearch}
-        />
+        <TextField id="search" label="Search" type="search" value={searchText} onChange={handleSearch} />
       </div>
       <TableContainer id="articlepending" component={Paper}>
         <Table className={classes.table} aria-label="sticky table">
-          <TableHead className='th'>
+          <TableHead className="th">
             <TableRow>
-            <TableCell>id</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Article Name</TableCell>
-            <TableCell >Student Name</TableCell>
-            <TableCell >Roll Number</TableCell>
-            <TableCell >Type</TableCell>
-            <TableCell>Upvotes</TableCell>
-            <TableCell>DownVotes</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>Approve</TableCell>
-            <TableCell>Read</TableCell>
+              {/* <TableCell>id</TableCell> */}
+              <TableCell>Date</TableCell>
+              <TableCell>Article Name</TableCell>
+              <TableCell>Student Name</TableCell>
+              <TableCell>Roll Number</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Upvotes</TableCell>
+              <TableCell>DownVotes</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Approve</TableCell>
+              <TableCell>Read</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {filteredRows.map((row) => (
               <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
+                {/* <TableCell component="th" scope="row">
                   {row.id}
-                </TableCell>
-                <TableCell>{row.date}</TableCell>
-                <TableCell>{row.article_name}</TableCell>
-                <TableCell>{row.student_name}</TableCell>
-                <TableCell>{row.roll_no}</TableCell>
-                <TableCell>{row.type}</TableCell>
-                <TableCell>{row.upvotes}</TableCell>
-                <TableCell>{row.downvotes}</TableCell>
+                </TableCell> */}
+                <TableCell>{row.timestamp ? row.timestamp.toDate().toLocaleDateString() : ''}</TableCell>
+                <TableCell>{row.title}</TableCell>
+                <TableCell>{row.workBy}</TableCell>
+                <TableCell>{row.rollNo}</TableCell>
+                <TableCell>{row.category}</TableCell>
+                <TableCell>{row.upVote}</TableCell>
+                <TableCell>{row.downVote}</TableCell>
                 <TableCell>
                   <span className={`status ${row.status}`}>{row.status}</span>
                 </TableCell>
                 <TableCell>{row.approve}</TableCell>
                 <TableCell>
-                  <Button variant="outlined" color="primary" href="#outlined-buttons">
+                  <Button variant="outlined" color="primary" onClick={() => handleRead(row.fileUrl)}>
                     READ
                   </Button>
                 </TableCell>
--- src/magazine/admindash/dashcomponent/review/Review.jsx
+++ src/magazine/admindash/dashcomponent/review/Review.jsx
@@ -1,100 +1,220 @@
-import './review.scss'
-import React, { useState } from 'react';
-import Navbar from '../Nav/Nav'
-import Sidebar from '../dashSidebar/Dashsidebar'
+import './review.scss';
+import React, { useState, useEffect } from 'react';
+import Navbar from '../Nav/Nav';
+import Sidebar from '../dashSidebar/Dashsidebar';
 import { DataGrid } from '@mui/x-data-grid';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import Button from '@mui/material/Button';
-
-
+import { collection, getDocs, onSnapshot, updateDoc, doc } from 'firebase/firestore';
+import { db } from '../../../../firebase';
 
 const Review = () => {
-   const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState('');
+  const [rows, setRows] = useState([]);
 
-const handleSearchTextChange = (e) => {
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'article'));
+        const fetchedRows = querySnapshot.docs.map((doc) => {
+          const data = doc.data();
+          return {
+            id: doc.id,
+            title: data.title,
+            artist: data.workBy,
+            category: data.category,
+            upcount: data.upVote,
+            downcount: data.downVote,
+          };
+        });
+        setRows(fetchedRows);
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      }
+    };
+
+    const unsubscribe = onSnapshot(collection(db, 'article'), (snapshot) => {
+      const fetchedRows = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        return {
+          id: doc.id,
+          title: data.title,
+          artist: data.workBy,
+          category: data.category,
+          upcount: data.upVote,
+          downcount: data.downVote,
+        };
+      });
+      setRows(fetchedRows);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleSearchTextChange = (e) => {
     setSearchText(e.target.value);
-}
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'title', headerName: 'Title', width: 200 },
-        { field: 'artist', headerName: 'Artist', width: 150 },
-        { field: 'category', headerName: 'Category', width: 100 },
-        { field: 'upcount', headerName: 'Upvotes', width: 100 },
-        { field: 'downcount', headerName: 'Downvotes', width: 100 },
-        {
-            field: 'vote',
-            headerName: 'Vote',
-            width: 100,
-            renderCell: (params) => (
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <ThumbUpIcon
-                        style={{ color: '#00ff00', marginRight: 2 }}
-                    />
-                    <ThumbDownIcon
-                        style={{ color: 'red', marginLeft: 2 }}
-                    />
-                </div>
-            ),
-        },
-        {
-            field: 'accept', headerName: 'Accept', width: 100,
-            renderCell: (params) => (
-                <Button variant="outlined" style={{ color: "#00ff00" }} >Accept</Button>
+  };
 
-            )
-        },
-        {
-            field: 'reject', headerName: 'Reject', width: 100,
-            renderCell: (params) => (
-                <Button variant="outlined" style={{ color: "red" }}>Reject</Button>
+  const handleThumbUpClick = async (row) => {
+    const articleRef = doc(db, 'article', row.id);
 
-            )
-        },
-    ]
+    try {
+      if (row.isUpvoted) {
+        // User already upvoted, so remove the upvote
+        await updateDoc(articleRef, { upVote: row.upcount - 1 });
+      } else {
+        // User didn't upvote, so increment the upvote count
+        await updateDoc(articleRef, { upVote: row.upcount + 1 });
 
-    const rows = [
-        {
-            id: 123,
-            title: "chatgpt",
-            artist: "shifan",
-            category: "Photograph",
-            upcount: 10,
-            downcount: 5,
-        },
-        {
-            id: 124,
-            title: "hello",
-            artist: "lalu",
-            category: "Story",
-            upcount: 7,
-            downcount: 8,
-        },
+        // If the user previously downvoted, decrement the downvote count
+        if (row.isDownvoted) {
+          await updateDoc(articleRef, { downVote: row.downcount - 1 });
+        }
+      }
 
-    ]
-    return (
-        <div className='review'>
-            <Sidebar />
-            <div className="reviewContainer">
-                <Navbar />
-                <div className="tableContainer">
-                    <div className="table" style={{ height: 400, width: '100%' }}>
-                        <h2 style={{ color: "lightgray" }}>Content Review</h2>
-                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', margin: 5 }}>
-                            <input type="text" value={searchText} onChange={handleSearchTextChange} placeholder='Search' />
-                        </div>
-                        <DataGrid
-                            rows={rows.filter(row => row.title.toLowerCase().includes(searchText.toLowerCase()))}
-                            columns={columns}
-                            pageSize={5}
-                            rowsPerPageOptions={[5]}
-                        />
-                    </div>
-                </div>
+      // Update the state
+      setRows((prevRows) =>
+        prevRows.map((prevRow) =>
+          prevRow.id === row.id
+            ? {
+                ...prevRow,
+                isUpvoted: !prevRow.isUpvoted,
+                upcount: prevRow.isUpvoted ? prevRow.upcount - 1 : prevRow.upcount + 1,
+                isDownvoted: false,
+                downcount: prevRow.isDownvoted ? prevRow.downcount - 1 : prevRow.downcount,
+              }
+            : prevRow
+        )
+      );
+    } catch (error) {
+      console.error('Error updating upVote:', error);
+    }
+  };
+
+
+  const handleThumbDownClick = async (row) => {
+    const articleRef = doc(db, 'article', row.id);
+
+    if (row.isDownvoted) {
+      return; // Disable the button if already downvoted
+    }
+
+    try {
+      await updateDoc(articleRef, { downVote: row.downcount + 1 });
+
+      // Disable the thumb-down button and update the state
+      setRows((prevRows) =>
+        prevRows.map((prevRow) =>
+          prevRow.id === row.id ? { ...prevRow, isDownvoted: true, downcount: prevRow.downcount + 1 } : prevRow
+        )
+      );
+
+      // If the thumb-up button was previously clicked, decrease the upvote count by 1
+      if (row.isUpvoted) {
+        await updateDoc(articleRef, { upVote: row.upcount - 1 });
+        setRows((prevRows) =>
+          prevRows.map((prevRow) =>
+            prevRow.id === row.id ? { ...prevRow, isUpvoted: false, upcount: prevRow.upcount - 1 } : prevRow
+          )
+        );
+      }
+    } catch (error) {
+      console.error('Error updating downVote:', error);
+    }
+  };
+
+
+  const handleAcceptClick = async (row) => {
+    const articleRef = doc(db, 'article', row.id);
+    try {
+      await updateDoc(articleRef, { approve: 1 });
+    } catch (error) {
+      console.error('Error updating approve:', error);
+    }
+  };
+
+  const handleRejectClick = async (row) => {
+    const articleRef = doc(db, 'article', row.id);
+    try {
+      await updateDoc(articleRef, { approve: -1 });
+    } catch (error) {
+      console.error('Error updating approve:', error);
+    }
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'title', headerName: 'Title', width: 200 },
+    { field: 'artist', headerName: 'Artist', width: 150 },
+    { field: 'category', headerName: 'Category', width: 100 },
+    { field: 'upcount', headerName: 'Upvotes', width: 100 },
+    { field: 'downcount', headerName: 'Downvotes', width: 100 },
+    {
+        field: 'vote',
+        headerName: 'Vote',
+        width: 100,
+        renderCell: (params) => (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <ThumbUpIcon
+              style={{ color: '#00ff00', marginRight: 2 }}
+              onClick={() => handleThumbUpClick(params.row)}
+              disabled={params.row.isUpvoted} // Disable the button if already upvoted
+            />
+            <ThumbDownIcon
+              style={{ color: 'red', marginLeft: 2 }}
+              onClick={() => handleThumbDownClick(params.row)}
+              disabled={params.row.isDownvoted} // Disable the button if already downvoted
+            />
+          </div>
+        ),
+      },
+    {
+      field: 'accept',
+      headerName: 'Accept',
+      width: 100,
+      renderCell: (params) => (
+        <Button variant="outlined" style={{ color: '#00ff00' }} onClick={() => handleAcceptClick(params.row)}>
+          Accept
+        </Button>
+      ),
+    },
+    {
+      field: 'reject',
+      headerName: 'Reject',
+      width: 100,
+      renderCell: (params) => (
+        <Button variant="outlined" style={{ color: 'red' }} onClick={() => handleRejectClick(params.row)}>
+          Reject
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <div className="review">
+      <Sidebar />
+      <div className="reviewContainer">
+        <Navbar />
+        <div className="tableContainer">
+          <div className="table" style={{ height: 400, width: '100%' }}>
+            <h2 style={{ color: 'lightgray' }}>Content Review</h2>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', margin: 5 }}>
+              <input type="text" value={searchText} onChange={handleSearchTextChange} placeholder="Search" />
             </div>
+            <DataGrid
+              rows={rows.filter((row) => row.title.toLowerCase().includes(searchText.toLowerCase()))}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+            />
+          </div>
         </div>
-    );
-    
-}
+      </div>
+    </div>
+  );
+};
 
-export default Review
+export default Review;
